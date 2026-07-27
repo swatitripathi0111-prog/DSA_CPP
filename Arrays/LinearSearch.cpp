909584f2cbba612b789ddc0eaeb0c9f919d713e9
swatitripathi0111-prog <swatitripathi0111@gmail.com>
2026-07-27 18:15:27 +0530

    Add linear search solution

---
+++ Arrays/LinearSearch.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int LinearSearch(int *arr,int n,int key){
+
+    for(int i=0;i<n;i++){
+    if(arr[i] == key){
+        return i;
+    }
+    }
+    return -1;
+}
+int main(){
+int arr[8] = {2,4,6,8,10,12,14,16};
+cout<<LinearSearch(arr,8,10);
+ return 0;
+}
