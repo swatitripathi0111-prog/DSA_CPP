cb8434125492416eb878def3a6859483f1fe6ef4
swatitripathi0111-prog <swatitripathi0111@gmail.com>
2026-08-19 01:43:47 +0530

    Add Reversewords in string solution

---
+++ Strings/ReverseWordsInString.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+string reverseWords(string s) {
+    int n = s.size();
+    string ans = "";
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++){
+     string word = "";
+     while(i<n && s[i] != ' '){
+        word += s[i];
+        i++;
+     }
+     reverse(word.begin(),word.end());
+     if(word.length() > 0){
+        ans += " "+word;
+     }
+    }
+    return ans.substr(1);
+}
+int main(){
+string str = "the sky is blue";
+cout<<reverseWords(str);
+return 0;
+}
