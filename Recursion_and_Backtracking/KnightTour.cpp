e5a97b9131a532ef522742059224e0544f4b2ca1
swatitripathi0111-prog <swatitripathi0111@gmail.com>
2026-09-05 03:11:37 +0530

    Add Knight Tour Solution

---
+++ Recursion_and_Backtracking/KnightTour.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool isValid(vector<vector<int>>& grid,int r,int c,int n,int ExpVal){
+    if(r < 0 || c < 0 || r >= n || c >= n || grid[r][c] != ExpVal){
+        return false;
+    }
+    if(ExpVal == n*n-1){
+        return true;
+    }
+    int ans1 = isValid(grid,r-2,c+1,n,ExpVal+1);
+    int ans2 = isValid(grid,r-1,c+2,n,ExpVal+1);
+    int ans3 = isValid(grid,r+1,c+2,n,ExpVal+1);
+    int ans4 = isValid(grid,r+2,c+1,n,ExpVal+1);
+    int ans5 = isValid(grid,r+2,c-1,n,ExpVal+1);
+    int ans6 = isValid(grid,r+1,c-2,n,ExpVal+1);
+    int ans7 = isValid(grid,r-1,c-2,n,ExpVal+1);
+    int ans8 = isValid(grid,r-2,c-1,n,ExpVal+1);
+     
+    return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8;
+}
+bool checkValidGrid(vector<vector<int>>& grid) {
+    return isValid(grid,0,0,grid.size(),0);
+}
+int main(){
+vector<vector<int>> grid = {{0,3,6},{5,8,1},{2,7,4}};
+cout<<checkValidGrid(grid);//0
+ return 0;
+}
