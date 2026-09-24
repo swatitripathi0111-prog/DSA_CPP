5e3c56617505c03f299d57ccb9db4ee2136e9ab0
swatitripathi0111-prog <swatitripathi0111@gmail.com>
2026-09-24 17:34:13 +0530

    Add First Unique Character in a string Solution

---
+++ Stacks_and_Queue/FirstUniqueChar.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int firstUniqChar(string s){
+    int n = s.size();
+    int freq[26] = {0};
+    for(int i=0;i<n;i++){
+     freq[s[i]-'a']++;
+    }
+    for(int i=0;i<n;i++){
+     if(freq[s[i]-'a'] == 1){
+        return i;
+     }
+    }
+    return -1;
+ }
+int main(){
+string s = "leetcode";
+cout<<firstUniqChar(s)<<endl;
+}
